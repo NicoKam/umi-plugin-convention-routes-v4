acfaca1fc91817ea84327be54d57af07da940430
wang.kangning <wang.kangning@alibaba-inc.com>
2022-03-23 19:39:39 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,74 +1,100 @@
-# umi-plugin-convention-routes
+# umi-plugin-convention-routes-v4
 
-[![NPM version](https://img.shields.io/npm/v/umi-plugin-convention-routes.svg?style=flat)](https://npmjs.org/package/umi-plugin-convention-routes) [![NPM downloads](http://img.shields.io/npm/dm/umi-plugin-convention-routes.svg?style=flat)](https://npmjs.org/package/umi-plugin-convention-routes)
+[![NPM version](https://img.shields.io/npm/v/umi-plugin-convention-routes-v4.svg?style=flat)](https://npmjs.org/package/umi-plugin-convention-routes-v4) [![NPM downloads](http://img.shields.io/npm/dm/umi-plugin-convention-routes-v4.svg?style=flat)](https://npmjs.org/package/umi-plugin-convention-routes-v4)
 
-这是一个为`约定式路由`功能而做的插件，适用于`umijs@3.x`
+这是一个为`约定式路由`功能而做的插件，适用于`umijs@4.x`
 
 它会代替`umijs`原有的`约定式路由`逻辑。
 
+如果你要寻找适用于`umijs@3.x`的该插件，请查看[https://www.npmjs.com/package/umi-plugin-convention-routes](https://www.npmjs.com/package/umi-plugin-convention-routes)
+
 ## 更新记录
 
-### 1.0.x
+### 1.0.0
 
-- 更新 `routes-watcher` 到 `1.0.0`
-- 增加动态可选路由支持
-
-### 0.1.x
-
-- feat: 增加动态路由功能（参考[umijs 的动态路由](https://umijs.org/docs/convention-routing#%E5%8A%A8%E6%80%81%E8%B7%AF%E7%94%B1)
-- fix: 修复 umi@3.2.6+ 版本使用插件后无法启动的问题
+- 初始版本
 
 ## Why
 
-`umijs`本身带有约定式路由的功能，为什么我要重新做一个？这就要从我的路由编写习惯说起了。
+关于编写该插件的原因，我在上个版本的插件的[Readme](https://www.npmjs.com/package/umi-plugin-convention-routes)中已经写的比较详细了。感兴趣可以点进去查看。
 
-### 以前的路由书写习惯
+## Convention
 
-在`umijs2.x`及之前，除了特殊的目录和文件，约定式路由会扫描所有的`js`，`ts`文件作为页面。并使用该文件的`目录路径`+`文件名`作为路由名称，其中如果是`index`文件，则省略`文件名`。
+本插件的路由约定规则如下。
 
-这种路由规则，其实是最接近传统多页应用的。但是这也带来一个麻烦的问题，就是页面文件比较杂乱（因为页面可能还会带一个样式文件），不方便在`pages`下添加其它内容/子模块（因为默认会被识别为页面)。
+### Entry
 
-所以我们约定了，只能识别`index`作为页面入口，以约束路由规则。在这种模式下，一个目录中只会包含一个页面文件及样式文件，所有和该页面相关的子模块/组件，也能够存放在该目录的`components`目录下，相关的`model`，`services`也是同理。如果是本页的特殊配置（比如复杂列表的表头），也可以在目录下单开一个文件存放。以目录的方式将页面按功能分隔开来，在后续维护升级的时候也更加方便。
+页面的入口文件必须为`index`，如`src/pages/home/index.tsx`，当然我还是推荐三件套（index + 页面本体 + 样式），这样更便于开发时查找文件及 `css-module` 自动命名。我相信你不会希望在调试样式的时候看到的都是 `index_xxxx`。
 
-所以，我们约定了以下规则，以进一步约束`约定式路由`。
+我们推荐使用命令行工具一件生成上面所说的`三件套`。
 
-1. 不再识别所有`js`文件，只能识别`index`文件作为页面入口。 -- 解决不便于添加额外内容的问题（比如复杂列表，其表头的描述就可以单独提取一个文件存放）
-2. `index`文件不编写任何业务内容，只引用真实的页面文件并`export`。 -- 解决编辑器文件定位问题，编辑`/home`页面，还是查找`home.jsx`文件（不然全是`index`，定位起来很难受吧）
-3. 所有本页面相关的子模块/组件，model，业务接口，都存放在当前页面目录的`components`, `models`, `services`子目录中。 -- 给所有资源都添加作用域的概念，避免所有资源都定义在全局，等项目迭代多了，有用的没用的都在`src/components`目录里。
+> TODO 开放命令行工具
 
-所以对于一个页面`/home`，至少会存在以下文件：
+### Pathname
 
+路径必须使用 小写字母 + `-` 构成，如果写了大写字母，默认是不被识别的。
+
+例如，该文件`src/pages/Home/index.tsx`就无法被识别为路由。
+
+如果你的目录名称由多个单词构成，请使用 `-` 分隔，如 `src/pages/user-list/index.tsx`。
+
+### Nested Routing
+
+本插件遵循了 `umi@2/3` 的嵌套路由习惯，在某个页面目录下建立 `_layout.tsx` 文件，可以让 `layout` 中的内容作用于所有子路由中。
+
+例如，`src/pages/home/_layout.tsx` 中编写的内容，会在 `/home` 以及 `/home/*` 路由中生效。其中 `layout` 组件的 `children` 代表子路由匹配到的页面组件。（在`react-router@6`中，`children` 的默认值为 `<Outlet />`
+
+下面是一个 `layout` 文件的编写示例：
+
+```jsx
+// src/pages/home/_layout.tsx
+
+// 该文件会被包含在 /home 及 /home/* 页面中
+export default (props) => {
+  const {children} = props
+  return (
+    <div>
+      <div>This is Layout</div>
+      {children}
+    </div>
+  );
+}
 ```
-/pages/home/index.js  -- 页面入口
-/pages/home/Home.jsx  -- 页面本体
-/pages/home/Home.less  -- 页面样式
+
+### Dynamic Routing
+
+你可以将路由中间的目录命名为 `[xxx]` 作为动态路由。
+
+例如：`src/pages/users/[id]/index.tsx` 将转换为路由 `/users/:id`。
+
+你可以使用 `useParams` 获取该动态数据：
+
+```tsx
+// src/pages/users/[id]/index.tsx
+import {useParams} from 'react-router';
+
+export default () => {
+  // 当访问 /users/123 时，这里的 id === '123'
+  const {id} = useParams<'id'>();
+  return <div>{id}/div>;
+}
 ```
 
-本页面的资源，可以这样定义：
+### Exclude
 
-```
-/pages/home/components/XXX -- 首页专用的组件/模块
-/pages/home/models/home.js -- 首页的dva-model
-/pages/home/columns.js -- 可能是当前页面的表格头部定义
-```
+部分特殊目录下的内容不会被识别为路由，目前会忽略的目录为 `components`, `models`, `services`, `layouts`。
 
-### umijs@3 的路由做了哪些改变
+例如，`src/pages/home/components/*` 下的所有内容都无法识别为路由。
 
-<p><img src="./assets/convention-routes-umi3.png" /></p>
+### 404
 
-我们可以[查看文档](https://umijs.org/docs/convention-routing)发现，umijs 默认对路由增加了一些限制。
-
-上面提到的`文件内容不包含 JSX 元素`不作为路由解析的规则，与我将`index`作为页面入口的规则相互冲突。因此我编写了这个插件，代替`umijs`以行使我的路由识别规则。
-
-### 注意事项
-
-如果你也赞同我上面的约定式路由规则，欢迎尝试本插件。否则请使用`umijs`的默认规则。
+建立 `src/pages/404.tsx` 后，当访问到无法匹配的路由时，将会展示该 404 页面的内容。
 
 ## Install
 
 ```bash
 # npm or yarn
-$ npm install umi-plugin-convention-routes -D
+$ npm install umi-plugin-convention-routes-v4 -D
 ```
 
 ## Usage
@@ -77,7 +103,7 @@ Configure in `.umirc.js`,
 
 ```js
 export default {
-  plugins: [],
+  plugins: ['umi-plugin-convention-routes-v4'],
 };
 ```
 
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,65 @@
 import { join } from 'path';
-import { scanRoutes } from 'routes-watcher';
-import { IApi } from 'umi';
-import { IRoute } from '@umijs/core';
 import fs from 'fs';
+import { scanRoutes } from 'routes-watcher';
+import { IApi, IRoute } from 'umi';
+
+type RevMapOptions<T> = {
+
+  /** 子节点的 key */
+  childKey?: string;
+
+  /** 是否优先遍历子节点 */
+  childrenFirst?: boolean;
+  parent?: T;
+};
+
+
+/**
+ * 遍历树状结构
+ * @param data 树状数据
+ * @param callback 返回值处理
+ * @param options 配置
+ */
+function revMap<T extends Object, R extends Object>(
+  data: T[],
+  callback: (d: T, parent: T | undefined, index: number) => R | undefined,
+  options: RevMapOptions<T> = {},
+): R[] {
+  const { childrenFirst, childKey = 'children', parent } = options;
+  return data.map((datum, index) => {
+    if (!datum) return datum; // error object
+    const children = datum[childKey] as T[] | undefined;
+    let newDatum: R | undefined;
+    if (childrenFirst) {
+      let newChildren: R[] = [];
+      if (Array.isArray(children)) {
+        newChildren = revMap(children, callback, { ...options, parent: datum });
+      }
+      newDatum = callback(
+        {
+          ...datum,
+          [childKey]: newChildren,
+        },
+        parent,
+        index,
+      );
+    } else {
+      newDatum = callback({ ...datum }, parent, index);
+      if (newDatum && Array.isArray(children)) {
+        // @ts-expect-error
+        newDatum['children'] = revMap(children, callback, { ...options, parent: newDatum });
+      }
+    }
+
+    return (newDatum ?? datum) as R;
+  });
+}
 
 type RouteConfig =
   | {
-      children: RouteConfig[];
-      [key: string]: any;
-    }
+    children: RouteConfig[];
+    [key: string]: any;
+  }
   | IRoute;
 
 function sortDynamicRoutes(arr: RouteConfig[] | undefined): IRoute[] {
@@ -56,13 +107,17 @@ export default (api: IApi) => {
   });
 
   let lastRoutesOutput = '';
-  let lastRoutesConfig: IRoute[] = [];
+  let lastRoutesConfig: Record<string, IRoute> = {};
 
-  api.modifyRoutes(async (routes: IRoute[]) => {
+  api.modifyRoutes(async (originRoutes) => {
+    const has404 =
+      fs.existsSync(join(api.paths.absPagesPath!, '404.js')) ||
+      fs.existsSync(join(api.paths.absPagesPath!, '404.tsx')) ||
+      fs.existsSync(join(api.paths.absPagesPath!, '404.jsx'));
+
     const { conventionRoutesConfig = {} } = api.config;
     const { successTips = 'Routes updated.', ...otherConventionRoutesConfig } = conventionRoutesConfig;
-
-    const newRoutes = await new Promise<IRoute[]>((r) => {
+    const newRoutes = await new Promise<Record<string, IRoute>>((r) => {
       scanRoutes({
         pageRoot: api.paths.absPagesPath,
         childrenKey: 'routes',
@@ -72,9 +127,9 @@ export default (api: IApi) => {
         excludes: [/[\\/](components|models|services|layouts)[\\/]/],
         modifyRoutePath: (path) => {
           return path.split('/').map((p) => {
-            if(/\[([^/^\[^\]]+)\]/.test(p)){
+            if (/\[([^/^\[^\]]+)\]/.test(p)) {
               let name = p.slice(1, p.length - 1);
-              if(p.endsWith('$]')){
+              if (p.endsWith('$]')) {
                 name = name.replace(/\$$/, '?');
               }
               return ':' + name;
@@ -88,7 +143,69 @@ export default (api: IApi) => {
         output: (outputStr: string) => {
           if (outputStr !== lastRoutesOutput) {
             lastRoutesOutput = outputStr;
-            lastRoutesConfig = sortDynamicRoutes(JSON.parse(outputStr));
+            const routesTree = sortDynamicRoutes(JSON.parse(outputStr));
+            const tmpRes: Record<string, IRoute> = {};
+            // console.log(JSON.stringify(routesTree, null, 2))
+            revMap(routesTree, (route, parent) => {
+              let id = route.path;
+              if (id === '/') id = '';
+              if (route.exact) id += '/index';
+              // else id += '/_layout';
+              if (id.startsWith('/')) id = id.slice(1);
+              let path = route.path === '/' ? route.path : route.path.slice(1);
+              if (parent?.id) {
+                path = path.replace(parent.id + '/', '');
+              }
+              let file = route.component.replace('@/pages/', '');
+              let layoutWritePath = '';
+              // isLayout
+              if (!route.exact) {
+                const tmpFilename = '_layout_' + id.replace(/[\/:]/g, '__') + '.tsx';
+                const layoutPath = join('plugin-convention-routes', tmpFilename);
+                const layoutFilePath = join(api.paths.absTmpPath, layoutPath);
+                layoutWritePath = layoutPath;
+                // api.writeTmpFile({
+                //   path: layoutPath,
+                //   content: `import { Outlet } from 'umi';
+                //   import Layout from '${route.path}';
+                //   return () => <Layout><Outlet /></Layout>;
+                //   `,
+                // })
+                file = layoutFilePath;
+                // add * to layout
+                tmpRes[id + '/*'] = {
+                  id: id + '/*',
+                  path: '*',
+                  parentId: id,
+                  file: has404 ? '404' : undefined,
+                }
+              }
+
+              const isIndex = parent && id === parent.id + '/index';
+              tmpRes[id] = {
+                id,
+                path: isIndex ? undefined : path,
+                file,
+                index: !!isIndex,
+                isLayout: !route.exact,
+                filePath: route.component,
+                layoutWritePath,
+              };
+              if (parent) {
+                tmpRes[id].parentId = parent.id;
+              } else if (originRoutes['@@/global-layout']) {
+                tmpRes[id].parentId = '@@/global-layout';
+              }
+
+
+
+              return {
+                id,
+                path,
+                file,
+              };
+            }, { childKey: 'routes' });
+            lastRoutesConfig = tmpRes;
             api.logger.info(successTips);
           }
           r(lastRoutesConfig);
@@ -96,27 +213,27 @@ export default (api: IApi) => {
         watch: false,
       });
     });
-    if (routes[0] && routes[0].component && routes[0].component.startsWith('@/layouts/index.')) {
-      const has404 =
-        fs.existsSync(join(api.paths.absPagesPath!, '404.js')) ||
-        fs.existsSync(join(api.paths.absPagesPath!, '404.tsx')) ||
-        fs.existsSync(join(api.paths.absPagesPath!, '404.jsx'));
-      const newRoute = [{ ...routes[0], routes: newRoutes }];
-      if (has404) {
-        type RouteConfig = typeof newRoute[0];
-        function add404(routes: RouteConfig['routes']) {
-          routes.forEach((route) => {
-            if (route.exact !== true && route.routes != null) {
-              add404(route.routes);
-            }
-          });
-          routes.push({ component: '@/pages/404' });
-        }
 
-        add404(newRoute);
-      }
-      return newRoute;
+    if (originRoutes['@@/global-layout']) {
+      newRoutes['@@/global-layout'] = originRoutes['@@/global-layout'];
     }
     return newRoutes;
   });
+
+
+  api.onBeforeCompiler(() => {
+    Object.values(lastRoutesConfig).forEach((route) => {
+      if (route.isLayout) {
+        // console.log(route);
+        api.writeTmpFile({
+          path: route.layoutWritePath,
+          noPluginDir: true,
+          content: `import { Outlet } from 'umi';
+                  import Layout from '${route.filePath}';
+                  export default () => <Layout><Outlet /></Layout>;
+                  `,
+        })
+      }
+    })
+  })
 };
